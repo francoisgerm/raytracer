38b33c2f19d81387ad72ce7fb7e921c188ebb619
Francois Germain <gfrancois@ensta.fr>
2016-04-23 11:14:11 +0200

    new misc.h

--- include/misc.h
+++ include/misc.h
@@ -4,21 +4,25 @@
 #include "geometry.h"
 #include "entry.h"
 
-typedef struct ssettings {
-	int depth;
-	double size_x, size_y, size_z; // scene's
-	color back;
-	double width, height; // screen's
-} settings;
 
-typedef struct sscreen {
-    point screenA;
-    point screenB;
-    point obs;
-    double width, height ;
-    point top_lefthand ; //point en haut a gauche de l'écran
-    vector i,j ;
-} screen ;
+typedef struct ssettings{
+	int depth;
+	double size_x, size_y, size_y; // scene's
+	color back;
+	source s;
+	point obs;
+	
+	flist* facets;
+
+	// screen
+	point screenA;
+	point screenB;
+	double width, height;
+	point top_lefthand ; //point en haut a gauche de l'écran
+  vector i,j ;
+
+
+} settings;
 
 double min3 (double a, double b, double c);
 double min2 (double a, double b);
