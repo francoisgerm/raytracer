291c914ec8ea5b40c804100e77bbc0d95b1027a7
Germain François <francoisgerm@gmail.com>
2016-04-23 10:34:48 +0200

    Update misc.h

--- include/misc.h
+++ include/misc.h
@@ -2,12 +2,23 @@
 #define MISC_H
 
 #include "geometry.h"
+#include "entry.h"
 
 typedef struct ssettings {
 	int depth;
 	double size_x, size_y, size_z; // scene's
+	color back;
 	double width, height; // screen's
 } settings;
+
+typedef struct sscreen {
+    point screenA;
+    point screenB;
+    point obs;
+    double width, height ;
+    point top_lefthand ; //point en haut a gauche de l'écran
+    vector i,j ;
+} screen ;
 
 double min3 (double a, double b, double c);
 double min2 (double a, double b);
